f6a7382da05bd734fdb85626edee8d276d996912
Marandil <me@marandil.pl>
2015-09-01 17:10:27 +0200

    Moved main test method to a separate file.

--- src/gtests/exceptions-tests.cpp
+++ src/gtests/exceptions-tests.cpp
@@ -107,9 +107,3 @@ TEST_F(ExceptionsTest, NotImplementedExceptionWhat)
     }
     EXPECT_TRUE(false); // Didn't catch
 }
-
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
